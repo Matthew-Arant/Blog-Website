d7f5159f637bf6af39527d50a6672d754d269ebf
Matthew Arant <rex.legorium@gmail.com>
2022-12-05 20:53:40 -0800

    Version 1.0

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const blogButton = window.document.getElementById('blogPost');
+blogButton.onclick = function() {
+    window.location.href = './post.html';
+}
+
+const homeButton = window.document.getElementById('middleHeader');
+homeButton.onclick = function() {
+    window.location.href = './home.html';
+}
---
+++ js/post-index.js
@@ -0,0 +1,9 @@
+const sideBarHome = window.document.getElementById('homePage');
+sideBarHome.onclick = function() {
+    window.location.href = './home.html';
+}
+
+const homeButton = window.document.getElementById('middleHeader');
+homeButton.onclick = function() {
+    window.location.href = './home.html';
+}
